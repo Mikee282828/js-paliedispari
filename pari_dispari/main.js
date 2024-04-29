b855a4d5442517fa336ee8f1506f818d03c3164c
michele laptop <micheledong2803@gmail.com>
2024-04-29 19:26:02 +0200

    pari dispari - function estrazione

---
+++ pari_dispari/main.js
@@ -0,0 +1,5 @@
+let scelta = prompt("Scegli pari o dispari");
+let sceltaNumero = parseInt(prompt("Inserisci un numero da 1 a 5"));
+function estrazione(){
+    return Math.round((Math.random)*5)
+}
